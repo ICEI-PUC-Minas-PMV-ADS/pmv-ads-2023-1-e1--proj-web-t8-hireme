433402d1c9a998e20178b504450685526923dccf
Gabriela Penaforte <devgabrielapenaforte@gmail.com>
2023-06-01 21:56:40 -0300

    coloquei o botão de dica na página de modelos

---
+++ src/js/modelos-curriculos.js
@@ -0,0 +1,9 @@
+function showConteudo(){
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.style.display = "inline-grid";
+}
+
+function hideConteudo(){
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.style.display = "none";
+}
