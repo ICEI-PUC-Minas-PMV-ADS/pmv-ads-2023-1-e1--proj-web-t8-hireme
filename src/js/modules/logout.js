2e628da488750e8683500b604e98b739bd6a4dcf
Victor Bastos <vbastosmaciel@outlook.com.br>
2023-06-10 11:38:06 -0300

    Ajustando botão de sair no menu responsivo

--- src/js/modules/logout.js
+++ src/js/modules/logout.js
@@ -1,7 +1,9 @@
 const LOGIN_URL = "../html/login.html";
 
-const logoutBtn = document.getElementById('logout');
-logoutBtn.addEventListener('click', function () {
-    sessionStorage.removeItem('usuario');
-    window.location.replace(LOGIN_URL);
-})
+const logoutBtn = document.querySelectorAll('#logout');
+for (let index = 0; index < logoutBtn.length; index++) {
+    logoutBtn[index].addEventListener('click', function() {
+        sessionStorage.removeItem('usuario');
+        window.location.replace(LOGIN_URL);
+    })
+}
