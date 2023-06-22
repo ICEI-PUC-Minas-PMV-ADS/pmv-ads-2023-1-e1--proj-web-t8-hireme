2d04b9452d5ee683b782b52e2f4709dc519e9b67
NikitaLelisCeccon <nikitaceccon@gmail.com>
2023-06-22 16:46:28 -0300

    ajustes pag perfil

--- src/js/recrutador.js
+++ src/js/recrutador.js
@@ -7,3 +7,32 @@ function menuShow(){
         menuMobile.classList.add('open');
     }
 }
+
+//FOTO
+const previewImage = document.getElementById('previewImage');
+const customUpload = document.getElementById('customUpload');
+
+uploadInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    previewImage.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+});
+
+customUpload.addEventListener('click', function() {
+  uploadInput.click();
+});
+
+
+//RETORNO DADOS CADASTRO
+let usuarioString = sessionStorage.getItem('usuario');
+let usuariojson = JSON.parse(usuarioString);
+
+window.addEventListener('load', function(){
+    document.getElementById("nome").innerHTML = usuariojson.nome;
+    document.getElementById("email").innerHTML = usuariojson.email;
+});
